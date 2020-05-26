647aeb52cb8705e6345b1c4b3b820b6f40421b1a
Jaap Joris Vens <jj@rtts.eu>
2020-05-26 20:24:14 +0200

    [ no commit message supplied ]

--- langlevedenbosch/models.py
+++ langlevedenbosch/models.py
@@ -25,16 +25,12 @@ class SectionImage(models.Model):
     class Meta:
         ordering = ['pk']
 
-class SectionTeaser(Numbered, models.Model):
+class SectionTeaser(models.Model):
     section = models.ForeignKey(Section, related_name='teasers', on_delete=models.CASCADE)
     title = VarCharField('titel')
-    position = models.PositiveIntegerField('positie', blank=True)
     image = models.ImageField(_('image'), blank=True)
     content = models.TextField(_('content'), blank=True)
     href = VarCharField(_('link'), blank=True)
 
-    def number_with_respect_to(self):
-        return self.section.teasers.all()
-
     class Meta:
-        ordering = ['position']
+        ordering = ['?']
